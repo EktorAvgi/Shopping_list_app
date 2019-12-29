bb0183719c5deacd6b3734b344f2ab03c0723d69
Ektor <aektoras@gmail.com>
2019-12-29 18:35:27 +0200

    Add simpl Shopping list app

---
+++ main.js
@@ -0,0 +1,123 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+// Set ENV
+process.env.NODE_ENV = "production";
+
+let mainWindow;
+let addwindow;
+
+// Listen for the app to be ready
+app.on("ready", function() {
+  // Create new window
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  // Load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Quit App when closed
+  mainWindow.on("closed", function() {
+    app.quit();
+  });
+
+  // Build the Menu from the Template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert the Menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle Create Add Window
+function createAddWindow() {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add Shopping List Item",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  // Load html into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Create Garbage collection
+  addWindow.on("close", function() {
+    addwindow = null;
+  });
+}
+
+// Catch item:add (Something happens here and I get error - quit and close)
+ipcMain.on("item:add", function(e, item) {
+  //   console.log(item); just to test in the Bash
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
+
+// Create a Menu Template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If mac, add empty object to menu
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools item if not in prod
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
